88eb026cebbda41a544be8f7c7ffd9b8eb55654d
marcel <m_harder88@yahoo.com>
2020-06-03 13:17:36 +0200

    alertify added

--- spa/src/app/soa/soa.component.ts
+++ spa/src/app/soa/soa.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: "app-soa",
@@ -7,12 +8,14 @@ import { Router } from "@angular/router";
   styleUrls: ["./soa.component.css"],
 })
 export class SoaComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private alertify: AlertifyService) {}
 
   ngOnInit(): void {}
   linkToCSD() { window.location.href = "http://77.173.53.32:8046";}
 
   showPresentatie(id: number) { if (id === 1) { window.open("https://docs.google.com/presentation/d/1YOepKNN-2SqzUhFmkZjrpH10m_P_vIdO5ksp9o09xHk/edit?usp=sharing"); }}
-  showDemo(id: number) { if (id === 1) { window.open("http://77.173.53.32:8086"); } }
+  showDemo(id: number) {
+    this.alertify.confirm("Your login credentials are: UN: marcel PWD: password", () => {if (id === 1) { window.open("http://77.173.53.32:8086"); }})
+ }
 
 }
--- spa/src/app/waitlist/waitlist.component.ts
+++ spa/src/app/waitlist/waitlist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-waitlist',
@@ -7,12 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WaitlistComponent implements OnInit {
 
-  constructor() { }
+  constructor(private alertify: AlertifyService) { }
 
   ngOnInit(): void {
   }
   linkToCSD() { window.location.href = "http://77.173.53.32:8046"; }
   showPresentatie(id: number) { if (id === 1) { window.open(""); }}
-  showDemo(id: number) { if (id === 1) { window.open("http://77.173.53.32:8083"); } }
+  showDemo(id: number) {
+     this.alertify.confirm("Your login credentials are: UN: marcel PWD: password", ()=>{if (id === 1) { window.open("http://77.173.53.32:8083"); }})
+  }
 
 }
