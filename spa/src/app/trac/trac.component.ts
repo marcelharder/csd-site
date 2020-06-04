cd4aed253178654157d217949c6403c7595949de
marcel <m_harder88@yahoo.com>
2020-06-04 10:55:05 +0200

    nog cardiohelp motivatie

--- spa/src/app/trac/trac.component.ts
+++ spa/src/app/trac/trac.component.ts
@@ -1,31 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AlertifyService } from "../_services/alertify.service";
 
 @Component({
-  selector: 'app-trac',
-  templateUrl: './trac.component.html',
-  styleUrls: ['./trac.component.css']
+  selector: "app-trac",
+  templateUrl: "./trac.component.html",
+  styleUrls: ["./trac.component.css"],
 })
 export class TracComponent implements OnInit {
-
   page = 1;
 
-  constructor() { }
+  constructor(private alertify: AlertifyService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ShowPage(no: number) {
+    if (no === 1) {
+      if (this.page === 1) {
+        return true;
+      }
+    }
+    if (no === 2) {
+      if (this.page === 2) {
+        return true;
+      }
+    }
+    if (no === 3) {
+      if (this.page === 3) {
+        return true;
+      }
+    }
+    if (no === 4) {
+      if (this.page === 4) {
+        return true;
+      }
+    }
+    if (no === 5) {
+      if (this.page === 5) {
+        return true;
+      }
+    }
+    return false;
   }
 
-  ShowPage(no: number){
-          if(no === 1){ if(this.page === 1){return true;}}
-          if(no === 2){ if(this.page === 2){return true;}}
-          if(no === 3){ if(this.page === 3){return true;}}
-          if(no === 4){ if(this.page === 4){return true;}}
-          if(no === 5){ if(this.page === 5){return true;}}
-          return false;
-  }
-
-  displayPage(no: number){
+  displayPage(no: number) {
     this.page = no;
   }
-  linkToCSD() { window.location.href = "http://77.173.53.32:8046"; }
-
+  linkToCSD() {
+    window.location.href = "http://77.173.53.32:8046";
+  }
+  showPresentatie(id: number) {
+    if (id === 1) {
+      window.open("");
+    } else {
+      this.alertify.message("Under development, check back ...");
+    }
+  }
+  showDemo(id: number) {
+    this.alertify.confirm(
+      "Your login credentials are: UN: marcel PWD: password",
+      () => {
+        if (id === 1) {
+          window.open("http://77.173.53.32:8088");
+        }
+      }
+    );
+  }
 }
